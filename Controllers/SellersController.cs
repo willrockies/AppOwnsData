9a475dfd84cd3cecbe26cdceb8ae20f511523aef
willrockies <wlsn.ads@gmail.com>
2023-03-03 15:25:18 -0300

    SellersController

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppOwnsData.Controllers
+{
+    public class SellersController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
