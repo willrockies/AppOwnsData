73a4e537c2020e4f57a594ff425512d7c48a97dd
willrockies <wlsn.ads@gmail.com>
2023-03-06 14:59:55 -0300

    Delete seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -42,5 +42,33 @@ namespace AppOwnsData.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        [AllowAnonymous]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
