49bde1ce404ce178c10e5b70e5afa6f58fe6c5fe
willrockies <wlsn.ads@gmail.com>
2023-03-06 15:15:59 -0300

    Seller details and eager loading

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -70,5 +70,23 @@ namespace AppOwnsData.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        [AllowAnonymous]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using AppOwnsData.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppOwnsData.Services
 {
@@ -26,7 +27,7 @@ namespace AppOwnsData.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id) ;
         }
 
         public void Remove(int id) 
